9344211e76c9e5540b4459d1f57c48d41d7922c1
Frc 8037 <frc8037@gmail.com>
2023-01-09 16:36:27 -0500

    End of day upload

---
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+
+public class SwerveModule {
+    
+    private final CANSparkMax driveMotor;
+    private final CANSparkMax turningMotor;
+
+    private final CANEncoder drivrEncoder;
+    
+
+}
